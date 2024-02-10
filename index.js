6aae72d4939a1b251de0e3e5c9e67db4d90bae85
Diego Surita <suritadev@gmail.com>
2024-02-10 12:41:28 -0300

    feat: add name insertion feature on people table and some other changes

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
-const http = require("node:http");
+import http from "node:http";
+import mysql from "mysql2/promise";
+import { faker } from "@faker-js/faker";
 
 const hostname = "0.0.0.0";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const connection = await mysql.createConnection({
+  host: "fullcycle-mysql",
+  user: "root",
+  password: "root",
+  database: "fullcycle",
+});
+
+const server = http.createServer(async (req, res) => {
+  connection.connect();
+
+  let response = `
+    <h1>Fullcycle Rocks!</h1>
+    <h3>People:</h3>
+    <ul>
+  `;
+
+  try {
+    const name = faker.person.fullName();
+
+    await connection.query(
+      `INSERT INTO people (name) VALUES ('${name.replace(/'/g, "\\'")}')`
+    );
+
+    console.log(`Person name ${name} inserted!`);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+
+  try {
+    const [results] = await connection.query("SELECT * FROM people");
+
+    results.forEach((person) => (response += `<li>${person.name}</li>`));
+  } catch (error) {
+    console.error("Error:", error);
+  }
+
+  response += "</ul>";
+
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("<h1>Fullcycle Rocks!</h1>");
+  res.setHeader("Content-Type", "text/html");
+  res.end(response);
 });
 
 server.listen(port, hostname, () => {
